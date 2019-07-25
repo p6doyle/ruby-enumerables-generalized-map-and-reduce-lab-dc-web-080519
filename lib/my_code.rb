f27b6aeec875fd049fcd918dd534d51688fad6f6
Peyton Doyle <p6doyle@gmail.com>
2019-07-25 19:30:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,12 +14,10 @@ def reduce (array, start = nil)
   if start
    total = start
   i = 0
-  end
 
   else 
     total = array[0
      i = 0
-  
   
   while i < array.length do
   total = yield(total, array[i])
